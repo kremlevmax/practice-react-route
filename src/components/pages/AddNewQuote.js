525356cd7a726c766ed874af1bdac21e3217e59c
kremlevmax <kremlevmax1989@gmail.com>
2021-08-03 23:59:57 -0700

    Custom http hook in progress

--- src/components/pages/AddNewQuote.js
+++ src/components/pages/AddNewQuote.js
@@ -2,14 +2,21 @@ import QuoteForm from "../quotes/QuoteForm";
 import useHttp from "../../hooks/use-http";
 import { addQuote } from "../../lib/api";
 import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
 
 const AddNewQuote = () => {
   const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
 
   const onAddQuote = (quote) => {
     sendRequest(quote);
   };
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotelist");
+    }
+  }, [status, history]);
   return (
     <>
       <QuoteForm onAddQuote={onAddQuote} />
